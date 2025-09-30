8f2d7e1ce8396457bb5fd202707cf75b47300f8f
B2103505 <phamvulamvip@gmail.com>
2025-09-30 20:27:07 +0700

    fix data

--- app/composables/ProductService.ts
+++ app/composables/ProductService.ts
@@ -1,9 +1,36 @@
+type Product = {
+  id: string
+  name: string
+  sku: string
+  price: number
+  price_original?: number
+  discount: number
+  sold: number
+  discount_rate: number
+  rating_average: number
+  image: string
+  createdAt: string
+}
+
+type Pagination = {
+  page: number
+  limit: number
+  total: number
+  totalPages: number
+}
+
+type ProductResponse = {
+  data: Product[]
+  pagination: Pagination
+}
+
 export const ProductService = () => {
   const config = useRuntimeConfig()
   const baseURL = config.public.apiBase
 
   const getProducts = async (page = 1) => {
-    return await $fetch(`/products?page=${page}`, { baseURL })
+    const res = await $fetch<ProductResponse>(`/products?page=${page}`, { baseURL })
+    return res.data
   }
 
   const searchProducts = async (params: {
@@ -18,12 +45,13 @@ export const ProductService = () => {
     const queryObj: Record<string, string> = {}
     Object.entries(params).forEach(([key, value]) => {
       if (value !== undefined && value !== null) {
-        queryObj[key] = String(value) // ép String
+        queryObj[key] = String(value)
       }
     })
 
     const query = new URLSearchParams(queryObj).toString()
-    return await $fetch(`/products/search?${query}`, { baseURL })
+    const res = await $fetch<ProductResponse>(`/products/search?${query}`, { baseURL })
+    return res.data
   }
 
   return { getProducts, searchProducts }
